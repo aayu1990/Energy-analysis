3caabe3f0398b70c9d132c477e521d6873adc854
Aayushi <ankurgupta@Ankurs-MacBook.local>
2018-08-02 14:39:41 -0700

    Updated all required serial IDs

--- Serials.py
+++ Serials.py
@@ -1,4 +1,35 @@
-series_ids = ['EMISS.CO2-C-CLEIB-CA.A',
-              'EMISS.CO2-TOTV-TT-TO-CA.A',
-              'EMISS.CO2-TOTV-TT-CO-CA.A',
-              'EMISS.CO2-TOTV-TT-NG-CA.A']
+series_ids = 
+#'EMISS.CO2-C-CLEIB-CA.A',
+#               'EMISS.CO2-TOTV-TT-TO-CA.A',
+#               'EMISS.CO2-TOTV-TT-CO-CA.A',
+#               'EMISS.CO2-TOTV-TT-NG-CA.A',
+            ['TOTAL.TETCBUS.A',#total primary energy Consumption(1950-2010) and total proimary energy Production(1950-2010)
+              #(Primary energy sources include fossil fuels (petroleum, natural gas, and coal), nuclear energy, 
+             #and renewable sources of energy).
+             'TOTAL.CLPRBUS.A',#total production of energy from coal(1950-2010)
+             'TOTAL.PAPRBUS.A', #total production of energy from crude oil(1950-2010)
+             'TOTAL.GETCBUS.A',#total production of energy from Geothermal(1950-2010)
+             'TOTAL.HVTCBUS.A',#total production of energy from Hydro(1950-2010)
+             'TOTAL.NLPRBUS.A',#total production of energy from Natural Gas,liquid(1950-2010)
+             'TOTAL.NGPRBUS.A',#total production of energy from Natural Gas,dry(1950-2010)
+             'TOTAL.NUETBUS.A',#total production of energy from Nuclear(1950-2010)
+             'TOTAL.SOTCBUS.A',#total production of energy from Solar(1950-2010)
+             'TOTAL.BMPRBUS.A',#total production of energy from Biomass(1950-2010)
+             'TOTAL.FFPRBUS.A',#total production of energy from Fossil fuels(1950-2010)
+             'STEO.HVEPCAP_US.A',#hydroelectric capacity for Electricity
+             'STEO.GEEPCAP_US.A',#Geothermal capacity for Electricity
+             'STEO.SOEPCAPX_US.A',#large scale solar capacity for Electricity
+             'STEO.WNEPCAPX_US.A',#wind capacity for Electricity
+             'STEO.HVCHCAP_US.A',#Hydroelectric-commercial and industrial sectors
+             'STEO.SOCHCAP_US.A',#large scale solar-commercial and industrial sectors
+             'STEO.WNCHCAP_US.A',#wind-commercial and industrial sectors
+             'TOTAL.TETCEUS.A',#total CO2 emissions
+             'TOTAL.TERCEUS.A'#resedential,commercial,Industrial, transportation,electric power,biomass sectorwise CO2 emission
+             'STEO.REECBUS.A',#Electric power consumption of renewable energy
+             'STEO.RECCBUS.A',#commercial consumption of renewable energy
+            'STEO.REICBUS.A',#industrial consumption of renewable energy
+             'STEO.RERCBUS.A',#resedential consumption of renewable energy
+             'STEO.REACBUS.A'#transportation consumption of renewable energy 
+            'STEO.RETCBUS.A',#total consumption of renewable energy by all sectors,annual
+              ]
+        
